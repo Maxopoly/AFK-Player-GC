ad4959fe66d0e580acd0ed87c92c219f7cbbf969
maxopoly <maxseinbot@gmx.de>
2015-08-02 00:01:04 +0200

    update

---
+++ src/com/github/Kraken3/AFKPGC/AFKPGC.java
@@ -0,0 +1,112 @@
+package com.github.Kraken3.AFKPGC;
+
+import java.io.File;
+import java.util.logging.Logger;
+import java.util.*;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.Kraken3.AFKPGC.commands.CommandHandler;
+
+public class AFKPGC extends JavaPlugin {
+	public static CommandHandler commandHandler;
+	public static Logger logger;
+	public static JavaPlugin plugin;
+	public static boolean enabled = true;
+	public static Set<UUID> immuneAccounts;
+	public static boolean debug = false;
+	public static BotDetector detector; // TODO: Might regret this. Adding for now.
+
+	@Override
+	public void onEnable() {
+		// setting a couple of static fields so that they are available
+		// elsewhere
+		logger = getLogger();
+		plugin = this;
+		commandHandler = new CommandHandler(this);
+		BotDetector.banfile=new File(plugin.getDataFolder().getAbsolutePath()+"/banlist.txt");
+		BotDetector.parseBanlist();
+		// Reads Config.yml - false as an answer indicated unrecoverable error
+		AFKPGC.enabled = ConfigurationReader.readConfig();
+		if (!AFKPGC.enabled) {
+			logger.warning("Plugin is not running");
+		}
+
+		getServer().getPluginManager()
+				.registerEvents(new EventHandlers(), this);
+
+		// Checks whether to 'garbage collect' AFKers every 20 ticks (1
+		// seconds);
+		getServer().getScheduler().scheduleSyncRepeatingTask(this,
+				new Kicker(), 0, 20L); // TODO make this configurable
+
+		getServer().getScheduler().scheduleSyncRepeatingTask(this,
+				new Runnable() {
+					public void run() {
+						LastActivity.currentTime = System.currentTimeMillis();
+					}
+				}, 0, 1L);
+
+		detector = new BotDetector();
+
+		getServer().getScheduler().scheduleSyncDelayedTask(this,
+				detector, BotDetector.frequency);
+
+		// Because bukkit..
+		getServer().getScheduler().scheduleSyncRepeatingTask(this,
+				new Runnable() {
+					public void run() {
+						LastActivity.FixInconsistencies();
+					}
+				}, 0, 6000L); // TODO make this configurable
+		
+		logger.info("AFK Player Garbage Collector has been enabled");
+	}
+
+	@Override
+	public void onDisable() {
+		BotDetector.freeEveryone(); // players wont get freed if the server
+									// crashed, should the bans of this plugin
+									// also be stored in an external file?
+		logger.info( "AFK Player Garbage Collector has been disabled");
+	}
+
+	public static void removerPlayer(UUID uuid) {
+		if (LastActivity.lastActivities.containsKey(uuid)) {
+			LastActivity.lastActivities.remove(uuid);
+		}
+	}
+
+	public static LastActivity addPlayer(Player p) {
+		if (p == null) {
+			return null;
+		}
+		if (AFKPGC.immuneAccounts != null && AFKPGC.immuneAccounts.contains(p.getUniqueId())) {
+			return null;
+		}
+		UUID uuid = p.getUniqueId();
+
+		LastActivity la;
+		if (LastActivity.lastActivities.containsKey(uuid)) {
+			la = LastActivity.lastActivities.get(uuid);
+		} else {
+			la = new LastActivity();
+			LastActivity.lastActivities.put(uuid, la);
+			la.playerName = uuid;
+			la.timeOfLastKickerPass = LastActivity.currentTime;
+		}
+		la.timeOfLastActivity = LastActivity.currentTime;
+		return la;
+	}
+
+	public static void debug(Object...msg) {
+		if (AFKPGC.debug) {
+			StringBuffer sb = new StringBuffer();
+			for (Object obj : msg) {
+				sb.append(obj.toString());
+			}
+			logger.info("DEBUG: " + sb.toString());
+		}
+	}
+}
---
+++ src/com/github/Kraken3/AFKPGC/EventHandlers.java
@@ -0,0 +1,124 @@
+package com.github.Kraken3.AFKPGC;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.*;
+
+class EventHandlers implements Listener {
+
+	@EventHandler
+	public void PlayerKickEvent(PlayerQuitEvent event) {
+		if (AFKPGC.enabled) {
+			AFKPGC.removerPlayer(event.getPlayer().getUniqueId());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerQuitEvent(PlayerQuitEvent event) {
+		if (AFKPGC.enabled) {
+			AFKPGC.removerPlayer(event.getPlayer().getUniqueId());
+		}
+	}
+
+	public void registerActivity(Player p) {
+		if (AFKPGC.enabled) {
+			AFKPGC.addPlayer(p);
+		}
+	}
+
+	// EVENTS THAT REGISTER PLAYER ACTIVITY
+
+	@EventHandler
+	public void PlayerJoinEvent(PlayerLoginEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	// seemingly duplicate events are here for resiliency/defensive programming
+	// as the plugin used to crash for some unobvious reason. I hate it too.
+	@EventHandler
+	public void onPlayerLogin(PlayerLoginEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerMoveEvent(PlayerMoveEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	//TODO: Add replacement event if not already done?
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onPlayerChatEvent(PlayerChatEvent event) {
+		if (AFKPGC.enabled && BotDetector.acceptableTPS < TpsReader.getTPS()) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerInteractEvent(PlayerInteractEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerDropItemEvent(PlayerDropItemEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerToggleSneakEvent(PlayerToggleSneakEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerItemHeldEvent(PlayerItemHeldEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerChangedWorldEvent(PlayerChangedWorldEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onEnchantItemEvent(EnchantItemEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getEnchanter().getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onPrepareItemEnchantEvent(PrepareItemEnchantEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(event.getEnchanter().getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onInventoryClickEvent(InventoryClickEvent event) {
+		if (AFKPGC.enabled) {
+			registerActivity(Bukkit.getPlayer(event.getWhoClicked().getName()));
+		}
+	}
+
+}
